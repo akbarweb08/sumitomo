f03cfbc566978f2cd28bddd283feae367b4bbfe9
M Akbar <akbarweb08@gmail.com>
2026-07-20 22:14:42 +0700

    record

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/recordeddata', [\App\Http\Controllers\RecordedDataController::class, 'index'])->name('recordeddata.index');
     Route::get('/recordeddata/show', [\App\Http\Controllers\RecordedDataController::class, 'show'])->name('recordeddata.show');
     Route::get('/recordeddata/sketch/{lot}/{date}', [\App\Http\Controllers\RecordedDataController::class, 'viewRecordSketch'])->name('recordeddata.sketch');
+    Route::delete('/recordeddata/destroy', [\App\Http\Controllers\RecordedDataController::class, 'destroy'])->name('recordeddata.destroy');
     // Box Error & Pallet Error
     Route::get('/boxerror', [\App\Http\Controllers\BoxErrorController::class, 'index'])->name('boxerror.index');
     Route::post('/boxerror/fix', [\App\Http\Controllers\BoxErrorController::class, 'fix'])->name('boxerror.fix');
